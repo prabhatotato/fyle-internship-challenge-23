74b98d823b9ae13de1543107c381a8a8098e2958
Prabhat Pundeer <prabhat12pundeer@gmail.com>
2024-05-16 00:20:22 +0530

    methods of app.component.ts are defined

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,11 +7,46 @@ import { ApiService } from './services/api.service';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit{
+  user: any; // User data
+  repositories: any[] = []; // Repositories data
+  currentPage = 1;
+  perPage = 10;
+
   constructor(
     private apiService: ApiService
   ) {}
 
   ngOnInit() {
-    this.apiService.getUser('johnpapa').subscribe(console.log);
+    this.fetchUser('johnpapa');
+    this.fetchRepos('johnpapa');
+  }
+
+  fetchUser(username: string) {
+    this.apiService.getUser(username).subscribe(user => {
+      this.user = user;
+    });
+  }
+
+  fetchRepos(username: string) {
+    this.apiService.getRepos(username, this.currentPage, this.perPage).subscribe(repos => {
+      this.repositories = repos;
+    });
+  }
+
+  nextPage() {
+    this.currentPage++;
+    this.fetchRepos('johnpapa');
+  }
+
+  prevPage() {
+    if (this.currentPage > 1) {
+      this.currentPage--;
+      this.fetchRepos('johnpapa');
+    }
+  }
+
+  updatePerPage(perPage: number) {
+    this.perPage = perPage;
+    this.fetchRepos('johnpapa');
   }
 }
--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,19 +1,38 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { tap, throwError } from 'rxjs';
+import { Observable, throwError } from 'rxjs';
+import { catchError, tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
 
+  private readonly BASE_URL = 'https://api.github.com/users';
+
   constructor(
     private httpClient: HttpClient
   ) { }
 
-  getUser(githubUsername: string) {
-    return this.httpClient.get(`https://api.github.com/users/${githubUsername}`);
+  getUser(githubUsername: string): Observable<any> {
+    return this.httpClient.get(`${this.BASE_URL}/${githubUsername}`)
+      .pipe(
+        tap(data => console.log('Fetched user data:', data)),
+        catchError(this.handleError)
+      );
   }
 
-  // implement getRepos method by referring to the documentation. Add proper types for the return type and params 
+  getRepos(githubUsername: string, page: number = 1, perPage: number = 10): Observable<any> {
+    const url = `${this.BASE_URL}/${githubUsername}/repos?page=${page}&per_page=${perPage}`;
+    return this.httpClient.get<any>(url)
+      .pipe(
+        tap(data => console.log('Fetched repositories:', data)),
+        catchError(this.handleError)
+      );
+  }
+
+  private handleError(error: HttpErrorResponse): Observable<never> {
+    console.error('An error occurred:', error.message);
+    return throwError('Something went wrong; please try again later.');
+  }
 }
